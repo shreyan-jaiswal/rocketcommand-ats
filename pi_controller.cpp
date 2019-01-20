0b086504499cc504c6e2949fc4cd25a609cbe1d1
Nicolas-Iskos <46684817+Nicolas-Iskos@users.noreply.github.com>
2019-01-20 14:43:33 -0500

    Update pi_controller.cpp

--- pi_controller.cpp
+++ pi_controller.cpp
@@ -1,18 +1,15 @@
 #include <utility>
 #include "pi_controller.h"
+#include "Eigen/Eigen"
 
-pi_controller::pi_controller(double p_arg, double i_arg, double discrete_arg = 0.){
+pi_controller::pi_controller(double p_arg, double i_arg){
     p = p_arg;
     i = i_arg;
-    discrete = discrete_arg;
 }
 
-double pi_controller::control(std::pair<double, double> e){
-    double output = p * e.first + i * e.second;
-    if(discrete != 0.){
-        output = discrete * round(output/discrete);
-    }
+double pi_controller::control(Matrix<2,1 double> e){
+    double output = p * e(0,0) + i * e(1,0);
     if(output < 0.) return 0.;
-    if(output > 90.) return 90.;
+    if(output > 0.125) return 0.125;
     return output;
 }
